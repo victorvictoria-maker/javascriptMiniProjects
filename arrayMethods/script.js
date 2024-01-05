c0f5cf52d7d318417190a524a23c1280f6381845
victorvictoria-maker <victorvictoria0001@gmail.com>
2024-01-05 21:30:47 +0100

    Learnt new array methods

---
+++ arrayMethods/script.js
@@ -0,0 +1,104 @@
+const inventors = [
+  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
+  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
+  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
+  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
+  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
+  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
+  { first: "Max", last: "Planck", year: 1858, passed: 1947 },
+  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
+  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
+  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
+  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
+  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
+];
+
+// sort inventors by the year they have lived.
+inventors.sort((a, b) => (a.passed - a.year > b.passed - b.year ? -1 : 1));
+
+console.table(inventors);
+
+const people = [
+  "Bernhard, Sandra",
+  "Bethea, Erin",
+  "Becker, Carl",
+  "Bentsen, Lloyd",
+  "Beckett, Samuel",
+  "Blake, William",
+  "Berger, Ric",
+  "Beddoes, Mick",
+  "Beethoven, Ludwig",
+  "Belloc, Hilaire",
+  "Begin, Menachem",
+  "Bellow, Saul",
+  "Benchley, Robert",
+  "Blair, Robert",
+  "Benenson, Peter",
+  "Benjamin, Walter",
+  "Berlin, Irving",
+  "Benn, Tony",
+  "Benson, Leana",
+  "Bent, Silas",
+  "Berle, Milton",
+  "Berry, Halle",
+  "Biko, Steve",
+  "Beck, Glenn",
+  "Bergman, Ingmar",
+  "Black, Elk",
+  "Berio, Luciano",
+  "Berne, Eric",
+  "Berra, Yogi",
+  "Berry, Wendell",
+  "Bevan, Aneurin",
+  "Ben-Gurion, David",
+  "Bevel, Ken",
+  "Biden, Joseph",
+  "Bennington, Chester",
+  "Bierce, Ambrose",
+  "Billings, Josh",
+  "Birrell, Augustine",
+  "Blair, Tony",
+  "Beecher, Henry",
+  "Biondo, Frank",
+];
+
+//   sort with last name
+// console.log(peopleFullName);
+const arrangedName = people.sort((a, b) => {
+  const [alastName, afirstName] = a.split(", ");
+  const [blastName, bfirstName] = b.split(", ");
+  return alastName > blastName ? 1 : -1;
+});
+console.table(arrangedName);
+
+// sum up the instances of each
+const data = [
+  "car",
+  "car",
+  "truck",
+  "truck",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "car",
+  "truck",
+  "pogostick",
+];
+const transportaton = data.reduce((obj, item) => {
+  console.log(item, obj);
+  if (obj.item == undefined) {
+    obj.item = 0;
+  }
+  //   if (`obj.${item}` == undefined) {
+  //     console.log(`obj.${item}`);
+  //     obj.item = 0;
+  //   }
+  return obj;
+}, {});
+
+console.log(transportaton);
--- cssVariables/script.js
+++ cssVariables/script.js
@@ -3,13 +3,6 @@ const spaceInput = document.querySelector(".space");
 const blurInput = document.querySelector(".blur");
 const image = document.querySelector(".image");
 
-// console.log("hi");
-
-// colour.addEventListener("change", makeChange);
-
-// console.log(colourInput, spaceInput, blurInput);
 colourInput.addEventListener("input", (e) => {document.documentElement.style.setProperty("--primary-colour", `${e.target.value}`)});
-
 blurInput.addEventListener("input", (e) => document.documentElement.style.setProperty("--blur", `${e.target.value}px`));
-
 spaceInput.addEventListener("input", (e) => document.documentElement.style.setProperty("--padding", `${e.target.value}px`));
